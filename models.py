a2f1615c868058fcb3964ca6093b7bfe762e4598
23f1000932 <23f1000932@ds.study.iitm.ac.in>
2025-03-10 00:20:06 +0530

    modified admin dasbboard

--- models.py
+++ models.py
@@ -15,23 +15,36 @@ class User(db.Model):
     is_admin = db.Column(db.Integer, default=1)
     # scores = db.relationship('Score', backref='user', lazy=True)
 
-# class Subject(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False)
-#     description = db.Column(db.Text)
-#     chapters = db.relationship('Chapter', backref='subject', lazy=True, cascade='all, delete-orphan')
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable=False) 
+    discription = db.Column(db.String(100),  nullable = True)
+    chapter_name = db.relationship('Chapter', backref='Subject', lazy=True)
 
-# class Chapter(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False)
-#     description = db.Column(db.Text)
-#     subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
-#     quizzes = db.relationship('Quiz', backref='chapter', lazy=True, cascade='all, delete-orphan')
+class Chapter(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100), nullable=False)
+    discription = db.Column(db.String(100),  nullable = True)
+    no_of_Qusetion = db.Column(db.Integer, default = 1)
+    subject_id = db.Column(db.Integer, db.forigenKey('Subject.id'), nullable = False)
+    quizzes = db.relationship('Quiz', backref='Chapter', lazy=True)
 
-# class Quiz(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'), nullable=False)
-#     date_of_quiz = db.Column(db.Date)
-#     time_duration = db.Column(db.String(5))  # HH:MM format
-#     remarks = db.Column(db.Text)
-#     questions = db.relationship('Question', backref='quiz', lazy=True, cascade='all, delete-orphan')
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    chapter_id = db.Column(db.Integer, db.ForeignKey('Chapter.id'), nullable = False)
+    date_of_quiz = db.Column(db.Date)
+    time_duration = db.Column(db.string(10)) #HH:MM
+    remark = db.Column(db.String(100), nullable = True)
+    question = db.relationship('Question', backref = 'Quiz', lazy = True)
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    quiz_id = db.Column (db.Integer , db.ForigenKey("Quiz.id"), nullable = False)
+    q_statement = db.Column(db.String(100), nullable = False)
+    o_1 = db.Column(db.String(50), nullable = False)
+    o_2 = db.Column(db.String(50), nullable = False)
+    o_3 = db.Column(db.String(50), nullable = False)
+    o_4 = db.Column(db.String(50), nullable = False)
+    correct_option = db.Column(db.Integer, nullable = False)
+
+    
